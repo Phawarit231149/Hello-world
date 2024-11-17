00e1b8c17ee736c8277dd6a4db05920019c788da
Phawarit231149 <phavarisyatniyom@gmail.com>
2024-11-17 13:12:41 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+#https://medium.com/botnoi-classroom/computer-vision-ก่อนเข้าสู่ยุค-deep-learning-6a10e161d686
+#https://www.geeksforgeeks.org/pedestrian-detection-using-opencv-python/
+
+
+#https://thedatafrog.com/en/articles/human-detection-video/
+
+import numpy as np
+import cv2
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+cap = cv2.VideoCapture(0)
+while(True):
+    # reading the frame
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (640, 480))
+    # using a greyscale picture, also for faster detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+    # detect people in the image
+    # returns the bounding boxes for the detected objects
+    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
+
+    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
+    for (xA, yA, xB, yB) in boxes:
+        # display the detected boxes in the colour picture
+        cv2.rectangle(frame, (xA, yA), (xB, yB),(0, 255, 0), 2)
+    # displaying the frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        # breaking the loop if the user types q
+        # note that the video window must be highlighted!
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+# the following is necessary on the mac,
+# maybe not on other platforms:
+cv2.waitKey(1)
